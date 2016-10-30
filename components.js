9856708385d12adab05f13833d70e480b604a5d3
Kamil Warpechowski <k.warpechowski@hakama.pl>
2016-10-30 13:49:32 +0100

    gitignore

--- components.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var hello_world_1 = require('./src/hello-world/hello-world');
-exports.HelloWorld = hello_world_1.HelloWorld;
-var about_component_1 = require('./src/about/about.component');
-exports.AboutComponent = about_component_1.AboutComponent;
-//# sourceMappingURL=components.js.map
